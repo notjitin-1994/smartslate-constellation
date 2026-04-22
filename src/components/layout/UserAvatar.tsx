c978547145a9d0a8a17827bdf447f0ad1d7cbd74
notjitin-1994 <notjitin-1994@users.noreply.github.com>
2026-04-22 14:29:26 +0530

    fix: correct lucide-react import in UserAvatar.tsx

--- src/components/layout/UserAvatar.tsx
+++ src/components/layout/UserAvatar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from 'react';
-import { User } from '@lucide-react/icons';
+import { User } from 'lucide-react';
 import Image from 'next/image';
 
 interface UserAvatarProps {
